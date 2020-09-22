6a52b0d92cbd23f30e82dc90a13a36af9c6a4ea9
Michal 'vorner' Vaner <vorner@vorner.cz>
2020-09-22 15:16:46 +0200

    Command line & git integration
    * Add command line parsing
    * Read the data from git and parse those
    * Nicer error handling (contexts and such)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 use std::cmp;
 use std::collections::{BTreeMap, BTreeSet};
 use std::fmt::{Display, Formatter, Result as FmtResult};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use anyhow::{anyhow, Context, Error};
 use cargo_lock::package::{Name, Package, SourceId, Version};
 use cargo_lock::Lockfile;
 use either::Either;
-use git2::{Branch, Oid, Repository, Revspec};
+use git2::{Object, Oid, Repository};
 use itertools::{EitherOrBoth, Itertools};
+use structopt::StructOpt;
+use thiserror::Error;
 
 /*
  * FIXME: What will happen if package moves from one source to another? When it gets renamed?
  */
+
+/// Checking what changed about dependencies between versions.
+#[derive(Debug, StructOpt)]
+struct Opts {
+    /// Git range specifying the changes to inspect.
+    revspec: String,
+
+    /// Path to the lock file inside the repository.
+    ///
+    /// Assumes it is the same in both versions.
+    #[structopt(short = "p", long = "path", default_value = "Cargo.lock")]
+    path: PathBuf,
+
+    #[structopt(short = "g", long = "git-repo", default_value = ".")]
+    repo: PathBuf,
+}
 
 #[derive(Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
 struct Dep {
@@ -31,16 +49,33 @@ impl From<Package> for Dep {
     }
 }
 
+#[derive(Debug, Error)]
+#[error("Not a git blob")]
+struct NotBlob;
+
+fn snapshot_to_file_content(repo: &Repository, hash: Oid, path: &Path) -> Result<String, Error> {
+    let commit = repo.find_commit(hash)?;
+    let tree = commit.tree()?;
+    let tree_entry = tree.get_path(path)?;
+    let object = tree_entry.to_object(&repo)?;
+    let blob = object.as_blob().ok_or(NotBlob)?;
+    let content = blob.content();
+    String::from_utf8(content.to_owned()).map_err(|_| anyhow!("cannot pase as UTF-8"))
+}
+
 type Deps = BTreeMap<Name, Vec<Dep>>;
 
-fn process_lockfile<P: AsRef<Path>>(p: P) -> Result<Deps, Error> {
-    let mut f = Lockfile::load(&p)
-        .with_context(|| format!("Couldn't load lockfile {}", p.as_ref().display()))?;
-    f.packages.sort_unstable();
+fn process_lockfile(repo: &Repository, hash: Oid, path: &Path) -> Result<Deps, Error> {
+    let data = snapshot_to_file_content(repo, hash, path)
+        .with_context(|| format!("Couldn't find lock file {} in {}", path.display(), hash))?;
+
+    let mut lockfile: Lockfile = data.parse()
+        .with_context(|| format!("{} in {} is not valid lock file", path.display(), hash))?;
+    lockfile.packages.sort_unstable();
 
     let mut packages = Deps::new();
 
-    for pkg in f.packages {
+    for pkg in lockfile.packages {
         packages
             .entry(pkg.name.clone())
             .or_default()
@@ -96,44 +131,27 @@ fn find_vers_diff(old: Vec<Dep>, new: Vec<Dep>) -> impl Iterator<Item = Op> {
     let removed = removed.into_iter().map(Op::Remove);
     let added = added.into_iter().map(Op::Add);
 
-    common.chain(removed).chain(added)
+    removed.chain(common).chain(added)
 }
 
-fn snapshot_to_file_content(repo: &Repository, hash: Oid, path: &Path) -> Result<String, Error> {
-    let commit = repo.find_commit(hash)?;
-    let tree = commit.tree()?;
-    let tree_entry = tree.get_path(path)?;
-    let object = tree_entry.to_object(&repo)?;
-    let blob = object.as_blob().ok_or(anyhow!("not a blob"))?;
-    let content = blob.content();
-    String::from_utf8(content.to_owned()).map_err(|_| anyhow!("cannot pase as UTF-8"))
-}
-
-fn branch_to_file_content(repo: &Repository, branch: Branch, path: &Path) -> Result<String, Error> {
-    let reference = branch.into_reference();
-    let tree = reference.peel_to_tree()?;
-    let tree_entry = tree.get_path(path)?;
-    let object = tree_entry.to_object(&repo)?;
-    let blob = object.as_blob().ok_or(anyhow!("not a blob"))?;
-    let content = blob.content();
-    String::from_utf8(content.to_owned()).map_err(|_| anyhow!("cannot pase as UTF-8"))
-}
-
-fn range_to_contents(
-    repo: &Repository,
-    revspec: Revspec,
-    path: &Path,
-) -> Result<(String, String), Error> {
-    let old = revspec.from().ok_or(anyhow!("not a from spec"))?.id();
-    let new = revspec.to().ok_or(anyhow!("not a to spec"))?.id();
-    let old = snapshot_to_file_content(repo, old, path)?;
-    let new = snapshot_to_file_content(repo, new, path)?;
-    Ok((old, new))
-}
+#[derive(Error, Debug)]
+#[error("Not a git spec")]
+struct NotSpec;
 
 fn main() -> Result<(), Error> {
-    let old = process_lockfile("testdata/old.lock")?;
-    let new = process_lockfile("testdata/new.lock")?;
+    let opts = Opts::from_args();
+    let repo = Repository::open(&opts.repo)
+        .with_context(|| format!("Can't open git repo at {}", opts.repo.display()))?;
+
+    let revspec = repo.revparse(&opts.revspec)?;
+
+    let spec = |spec: Option<&Object<'_>>| {
+        let spec = spec.ok_or(NotSpec)?.id();
+        process_lockfile(&repo, spec, &opts.path)
+    };
+
+    let old = spec(revspec.from()).context("Failed to decode old version")?;
+    let new = spec(revspec.to()).context("Failed to decode new version")?;
 
     old.into_iter()
         .merge_join_by(new, |l, r| l.0.cmp(&r.0))
